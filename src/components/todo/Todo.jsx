09cf17c778c1d548785ed7e1fe7511d0fabe7221
oh <outputor777@gmail.com>
2022-07-26 18:38:45 +0900

    수정기능

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css';
 
-const Todo = ({ todos, setTodos, id, title, desc, isDone}) => {
+const Todo = ({ todos, setTodos, id, title, desc}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editTitle, setEditTitle] = useState(title);
+  const [editDesc, setEditDesc] = useState(desc);
+
   const handleDelete = (id) => {
     let newTodos = todos.filter((data) => data.id !== id);
     setTodos(newTodos);
@@ -19,22 +23,47 @@ const Todo = ({ todos, setTodos, id, title, desc, isDone}) => {
     localStorage.setItem('todoData', JSON.stringify(newTodos))
   }
 
-  return (
-    isDone === false ?
-    <div className="todo-box" >
-      <h2>{title}</h2>
-      <p>{desc}</p>
-      <button className='redBtn' onClick={() => {handleDelete(id)}}>삭제하기</button>
-      <button className='greenBtn' onClick={() => {handleCompleted(id)}}>완료</button>
-    </div>
-    :     
-    <div className="todo-box" >
-      <h2>{title}</h2>
-      <p>{desc}</p>
-      <button className='redBtn' onClick={() => {handleDelete(id)}}>삭제하기</button>
-      <button className='greenBtn' onClick={() => {handleCompleted(id)}}>취소</button>
-  </div>
-  )
+  const handleEdit = (id) => {
+    let newTodo = todos.map((data) => {
+      if(data.id === id) {
+        data.title = editTitle;
+        data.desc = editDesc;
+      }
+      return data;
+    })
+    setTodos(newTodo);
+    localStorage.setItem('todoData', JSON.stringify(newTodo))
+  }
+
+  const handleEditTitle = (e) => {
+    setEditTitle(e.target.value);
+  }
+
+  const handleEditDesc = (e) => {
+    setEditDesc(e.target.value);
+  }
+
+      if(!isEditing) {
+        return (      
+          <div className="todo-box" >
+            <h2>{title}</h2>
+            <p>{desc}</p>
+            <button className='btn' onClick={() => {setIsEditing(true)}}>수정하기</button>
+            <button className='redBtn' onClick={() => {handleDelete(id)}}>삭제하기</button>
+            <button className='greenBtn' onClick={() => {handleCompleted(id)}}>완료</button>
+          </div>
+        )
+      }else {
+        return (
+        <div className="todo-box" >
+          <input type="text" value={editTitle} onChange={handleEditTitle} placeholder="해야 할 일을 입력하세요" />
+          <input type='text' value={editDesc} onChange={handleEditDesc} placeholder="구체적으로 적어보세요"  />
+          <button className='btn' onClick={() => {setIsEditing(false); handleEdit(id)}}>수정완료</button>
+          <button className='redBtn' onClick={() => {handleDelete(id)}}>삭제하기</button>
+          <button className='greenBtn' onClick={() => {handleCompleted(id)}}>취소</button>
+        </div>
+        )
+      }
 }
 
 export default Todo
